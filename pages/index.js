c4d22b30bb8a041a1fa3a9869533f9c259a752fd
Maria Lisboa <zaza.naza95@gmail.com>
2024-05-10 15:03:42 +0000

    adicionado pagina inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Amigo, vamos ser os melhores no Fortnite!</h1>
+    return <h1>Amigo, vamos ser os melhores no Fortnite!!! Se voce concorda, dá uma risadinha!</h1>
 }
 
 export default Home;
