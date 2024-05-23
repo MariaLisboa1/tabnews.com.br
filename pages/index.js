095aaf39918895d70395a78c21643f1af135f1db
Maria Lisboa <zaza.naza95@gmail.com>
2024-05-23 22:14:59 +0000

    adicionado risos diferentes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Amigo, vamos ser os melhores no Fortnite!!! Se voce concorda, dá uma risadinha! rsrs</h1>
+    return <h1>Amigo, vamos ser os melhores no Fortnite!!! Se voce concorda, dá uma risadinha! heheheheh</h1>
 }
 
 export default Home;
