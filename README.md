1f277838821bb420fb29a4136863c0cda28bd70d
Maria Lisboa <zaza.naza95@gmail.com>
2024-10-30 22:43:13 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # tabnews.com.br
+
 Clonando o https://www.tabnews.com.br
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Amigo, vamos ser os melhores no Fortnite!!! Se voce concorda, dá uma risadinha! heheheheh</h1>
+  return (
+    <h1>
+      Amigo, vamos ser os melhores no Fortnite!!! Se voce concorda, dá uma
+      risadinha! heheheheh
+    </h1>
+  );
 }
 
 export default Home;
