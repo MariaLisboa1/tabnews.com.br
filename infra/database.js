b86167d7acfe35e0846b072329f34f09ace9813a
Maria Lisboa <zaza.naza95@gmail.com>
2025-10-20 23:41:03 +0000

    test for logs

--- infra/database.js
+++ infra/database.js
@@ -16,8 +16,9 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
   });
 
+  await client.connect();
+
   try {
-    await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
